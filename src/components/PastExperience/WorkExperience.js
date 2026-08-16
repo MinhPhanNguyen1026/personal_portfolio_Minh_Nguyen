1fb8bf0bbf5c725b5d75f75720dd6201f4ddb7d2
Minh Nguyen <minhphannguyen1026@gmail.com>
2026-08-16 09:20:02 -0400

    Update to fix image not rendering

--- src/components/PastExperience/WorkExperience.js
+++ src/components/PastExperience/WorkExperience.js
@@ -127,9 +127,9 @@ const VOLUNTEER_EXPERIENCE = [
     status: "Volunteer",
     href: "https://dandyhacks.net",
     logo: {
-      src: "https://scontent-sjc3-1.cdninstagram.com/v/t39.30808-6/468531939_18209114839288662_4731406689385949607_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=106&ig_cache_key=MzQ2MTY4NTE4NTUyMTI0NDgwNw%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkZFRUQueHBpZHMuMTQ0MC5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=Yp3yC-55VtIQ7kNvwFbAM1r&_nc_oc=AdqIa04Crcj39FVR3APpmDrraeVR5vvQKnxOLvltBrid-vZXi1vZUU9P7ZaLjgYpJCA&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-sjc3-1.cdninstagram.com&_nc_gid=faX1N7cCxqZqlQWL3upORQ&_nc_ss=7a22e&oh=00_Af5zYFxkr0dw8h1x725EYN3ksUpbj6OW3YFFXkUYts-c3w&oe=6A187859",
-      width: 690,
-      height: 190,
+      src: `${process.env.PUBLIC_URL}/HackathonLogo.png`,
+      width: 640,
+      height: 640,
     },
   },
   {
